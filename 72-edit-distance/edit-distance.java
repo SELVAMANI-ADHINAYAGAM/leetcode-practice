4fb008e1fd8d30ff62279b84a235ac5ba949dfff
SELVAMANI <24106105@drngpit.ac.in>
2026-01-05 22:52:19 +0530

    Time: 5 ms (64.49%) | Memory: 47.3 MB (33.32%) - LeetSync

---
+++ 72-edit-distance/edit-distance.java
@@ -0,0 +1,31 @@
+ class Solution {
+    public static int minDistance(String str1, String str2) {
+        int rSize = str1.length();
+        int cSize = str2.length();
+        int[][] dp = new int[rSize+1][cSize+1];
+        
+        for(int row=0; row<=rSize; row++)
+            dp[row][0] = row;
+        for(int col=0; col<=cSize; col++)
+            dp[0][col] = col;
+        
+        for(int row=1; row<=rSize; row++) {
+            for(int col=1; col<=cSize; col++) {
+                if(str1.charAt(row-1) == str2.charAt(col-1))
+                    dp[row][col] = dp[row-1][col-1];
+                else {
+                    dp[row][col] = 1 + Math.min(
+                        dp[row-1][col-1],
+                        Math.min(dp[row-1][col], dp[row][col-1])
+                    );
+                }
+            }
+        }
+        return dp[rSize][cSize];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minDistance("horse", "ros"));
+        System.out.println(minDistance("intention", "execution"));
+    }
+}
