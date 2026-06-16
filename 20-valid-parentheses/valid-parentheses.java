a083d8e26aedf5112c2ba53a0b9b3e3b8876d2c7
SELVAMANI <selvamaniadhinayagam@gmail.com>
2026-06-16 21:48:54 +0530

    Time: 3 ms (87.59%) | Memory: 43.4 MB (40.36%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> sta=new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(ch=='['||ch=='{'||ch=='('){
+                sta.push(ch);
+            }
+            else{
+                if(sta.isEmpty()){
+                    return false;
+                }
+                char top=sta.pop();
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+
+                    return false;
+                }
+
+            }
+        }
+        return sta.isEmpty();
+
+    }
+}
